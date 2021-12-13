4129dc1a332b3073898ae0f5449b88f3cb84e1ea
rvogel <vogel@hallowelt.biz>
2021-12-13 16:15:21 +0100

    Remove legacy skin `BlueSpiceCalumma`

--- settings.d/080-BlueSpiceDiscovery.php
+++ settings.d/080-BlueSpiceDiscovery.php
@@ -4,7 +4,7 @@ wfLoadSkin( 'BlueSpiceDiscovery' );
 $GLOBALS['wgDefaultSkin'] = "bluespicediscovery";
 
 // TODO: Have logo in skin independent location
-if ( $GLOBALS['wgLogos']['1x'] == $GLOBALS['wgResourceBasePath'] . '/skins/BlueSpiceCalumma/resources/images/common/logo/bs3_logo.png' ){
+if ( $GLOBALS['wgLogos']['1x'] == $GLOBALS['wgResourceBasePath'] . '/resources/assets/wiki.png' ){
 	$GLOBALS['wgLogos'] = [
 		'1x' => $GLOBALS['wgResourceBasePath'] . '/skins/BlueSpiceDiscovery/resources/images/bs_logo.png'
 	];
