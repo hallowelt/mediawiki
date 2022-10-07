4f7317b9240114442f38939468f31d7bd1dbaf5c
rvogel <vogel@hallowelt.biz>
2022-10-07 11:11:41 +0200

    Add build and settings files and apply core file changes from previous `REL1_39`

---
+++ _bluespice/pre-autoload-dump.d/05-add_installer_overrides.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Helper script to load the installer into the proper directory
+# Copyright: 2021
+# License: GPLv3
+
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+PURPLE='\033[0;35m'
+NC='\033[0m'
+
+BRANCH=$(git branch | cut -f 2 -d " ")
+BRANCH=${BRANCH%%[[:space:]]}
+BRANCH=${BRANCH##[[:space:]]}
+BRANCH="${BRANCH//[$'\t\r\n ']}"
+
+if [ "$BRANCH" == "4.1.x" ] ||  [ "$BRANCH" == "4.2.x" ]
+then
+	BRANCH="REL1_35-$BRANCH"
+fi
+
+printf "\n${PURPLE}Fetching installer: ${NC}"
+
+if ! [ -d "mw-config/overrides/.git" ]
+then
+	rm -rf mw-config/overrides
+	git clone -b $BRANCH --depth 1 https://gerrit.wikimedia.org/r/bluespice/mw-config/overrides mw-config/overrides
+else
+	git -C mw-config/overrides/ pull
+fi
+
+printf "${GREEN}[ DONE ]${NC}\n"
---
+++ settings.d/001-BlueSpiceDistribution.php
@@ -0,0 +1,109 @@
+<?php
+
+wfLoadExtension( 'Arrays' );
+wfLoadExtension( 'CategoryTree' );
+wfLoadExtension( 'DynamicPageList3' );
+wfLoadExtension( 'HitCounters' );
+require_once __DIR__ . "/../extensions/ImageMapEdit/ImageMapEdit.php";
+wfLoadExtension( 'RSS' );
+wfLoadExtension( 'Echo');
+wfLoadExtension( 'TitleKey');
+require_once __DIR__ . "/../extensions/EmbedVideo/EmbedVideo.php";
+wfLoadExtension( 'FilterSpecialPages' );
+wfLoadExtension( 'UserMerge' );
+$wgUserMergeProtectedGroups = [];
+$wgUserMergeUnmergeable = [];
+wfLoadExtension( 'Variables' );
+wfLoadExtension( 'BlueSpiceEchoConnector' );
+wfLoadExtension( 'BlueSpiceDistributionConnector' );
+wfLoadExtension( 'UserFunctions' );
+$GLOBALS['wgUFAllowedNamespaces'] = array_fill( 0, 5000, true );
+wfLoadExtension( 'UrlGetParameters' );
+wfLoadExtension( 'FlexiSkin' );
+wfLoadExtension( 'Loops' );
+wfLoadExtension( 'InviteSignup' );
+$GLOBALS['wgGroupPermissions']['sysop']['invitesignup'] = true;
+$GLOBALS['bsgPermissionConfig']['invitesignup'] = [
+	'type' => 'global',
+	'roles' => [ 'admin' ]
+];
+wfLoadExtension( 'MenuEditor' );
+wfLoadExtension( 'EnhancedUpload' );
+$GLOBALS['wgEnhancedUploadOverrideStandardUpload'] = true;
+
+$GLOBALS['wgGroupTypes'] = [
+	'*'                => 'implicit',
+	'user'             => 'implicit',
+	'autoconfirmed'    => 'implicit',
+	'sysop'            => 'core-minimal',
+	'bureaucrat'       => 'core-extended',
+	'bot'              => 'core-extended',
+	'interface-admin'  => 'core-extended',
+	'suppress'         => 'core-extended',
+	'autoreview'       => 'extension-extended',
+	'editor'           => 'extension-minimal',
+	'review'           => 'extension-extended',
+	'reviewer'         => 'extension-minimal',
+	'smwcurator'       => 'extension-extended',
+	'smweditor'        => 'extension-extended',
+	'smwadministrator' => 'extension-extended',
+	'widgeteditor'     => 'extension-extended'
+];
+
+$bsImagesPath =
+	$GLOBALS['wgScriptPath']
+	. '/extensions/BlueSpiceDistributionConnector/resources/images';
+
+/*
+ * Use an other image for MediaWiki.org
+ */
+$GLOBALS['wgFooterIcons']['poweredby']['mediawiki'] = [
+	'src' => $bsImagesPath . '/footer/MediaWiki.png',
+	'height' => '27',
+	'width' => '149'
+] + $GLOBALS['wgFooterIcons']['poweredby']['mediawiki'];
+
+/*
+ * We want to use an other image for this extensions but the config files are processed to early.
+ * So we have to set the complete items.
+ */
+$GLOBALS['wgFooterIcons']['poweredby'] += [
+	'bluespice' => [
+		'src' => $bsImagesPath . '/footer/BlueSpice.png',
+		'url' => 'http://bluespice.com',
+		'alt' => 'Powered by BlueSpice',
+		'height' => '27',
+		'width' => '149'
+	]
+];
+
+if ( array_key_exists( 'semanticmediawiki', $GLOBALS['wgFooterIcons']['poweredby'] ) ) {
+	$footerIcons = [
+		'mediawiki' => $GLOBALS['wgFooterIcons']['poweredby']['mediawiki'],
+		'bluespice' => $GLOBALS['wgFooterIcons']['poweredby']['bluespice'],
+		'semanticmediawiki' => $GLOBALS['wgFooterIcons']['poweredby']['semanticmediawiki']
+	];
+
+	unset( $GLOBALS['wgFooterIcons']['poweredby']['mediawiki'] );
+	unset( $GLOBALS['wgFooterIcons']['poweredby']['bluespice'] );
+	unset( $GLOBALS['wgFooterIcons']['poweredby']['semanticmediawiki'] );
+
+	$footerIcons += $GLOBALS['wgFooterIcons']['poweredby'];
+
+	$GLOBALS['wgFooterIcons']['poweredby'] = $footerIcons;
+}
+
+$defaultLogoPath = $GLOBALS['wgResourceBasePath'] . '/resources/assets/wiki.png';
+$blueSpiceLogoPath = "$bsImagesPath/bs_logo.png";
+
+if ( $GLOBALS['wgLogos']['1x'] === $defaultLogoPath ) {
+	$GLOBALS['wgLogos'] = [ '1x' => $blueSpiceLogoPath ];
+}
+
+if ( $GLOBALS['wgFavicon'] == '/favicon.ico' ){
+	$GLOBALS['wgFavicon'] = "$bsImagesPath/favicon.ico";
+}
+
+unset( $bsImagesPath );
+unset( $defaultLogoPath );
+unset( $blueSpiceLogoPath );
