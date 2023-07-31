aa17b2b94c4e25780951194ba893dcdfd0705f60
rvogel <vogel@hallowelt.biz>
2023-07-31 11:06:29 +0200

    Switch to new Extension:EmbedVideo fork
    ERM33480
    
    [4.x]

--- settings.d/000-MWStakeComponents.php
+++ settings.d/000-MWStakeComponents.php
@@ -2,12 +2,12 @@
 
 /**
  * This is required to ensure that the MWStakeComponents have a chance to register their services
- * via `$wServiceWiringFiles`_before_ any other extension can instantiate the `MediaWikiServices`
+ * via `$wgServiceWiringFiles`_before_ any other extension can instantiate the `MediaWikiServices`
  * service container.
  *
- * Background: Even though it should not be done, some extensions (e.g. "Extension:EmbedVideo")
- * will make a call to `MediaWikiServices::getInstance` at "manifest.callback" time ( = "on 
- * registration" in `$IP/includes/Setup.php`)
+ * Background: Even though it should not be done, some extensions will make a call to
+ * `MediaWikiServices::getInstance` at "manifest.callback" time ( = "on registration"
+ * in `$IP/includes/Setup.php`)
  *
  * ERM23675
  */
