da85e9dc4d7d0862670078e461affc3268018a7c
rvogel <vogel@hallowelt.biz>
2023-12-01 16:20:38 +0100

    Enable Skin:Vector by default
    MediaWiki core references messages from Skin:Vector (e.g. `vector-view-view`)
    in `MessagesEn.php` which causes errors in the L10N cache if it is not enabled
    
    ERM35038
    
    [4.4.x]

--- settings.d/005-MediaWikiExtensions.php
+++ settings.d/005-MediaWikiExtensions.php
@@ -12,3 +12,10 @@ wfLoadExtension( 'Renameuser' );
 wfLoadExtension( 'SyntaxHighlight_GeSHi' );
 wfLoadExtension( 'WikiEditor' );
 wfLoadExtension( 'CodeEditor' );
+
+// ERM35038 - MediaWiki core references Skin:Vector specific messages
+// (e.g. `vector-view-view`) in `MessagesEn.php` which causes errors
+//  in the L10N cache if Skin:Vector is not enabled
+wfLoadSkin( 'Vector' );
+$GLOBALS['wgSkipSkins'][] = 'vector';
+
