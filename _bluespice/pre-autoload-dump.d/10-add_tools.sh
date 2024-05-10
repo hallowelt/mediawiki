52fece7af7fd3971e51b84d054ddcc25579d6898
rvogel <vogel@hallowelt.biz>
2024-05-10 16:07:19 +0200

    Add tools

---
+++ _bluespice/pre-autoload-dump.d/10-add_tools.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Helper script to load useful tools
+# Copyright: 2024
+# License: GPLv3
+
+targetdir="_bluespice/tools"
+
+if [ ! -d "$targetdir" ]
+then
+	mkdir -p "$targetdir"
+fi
+
+wget -q -O "$targetdir/mediawiki-adm" "https://github.com/hallowelt/misc-mediawiki-adm/releases/latest"
+chmod +x "$targetdir/mediawiki-adm"
+
+wget -q -O "$targetdir/parallel-runjobs-service" "https://github.com/hallowelt/misc-parallel-runjobs-service/releases/latest"
+chmod +x "$targetdir/parallel-runjobs-service"
