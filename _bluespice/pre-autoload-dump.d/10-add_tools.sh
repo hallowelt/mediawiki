8f1b9226b9b99f38eaeb3f3fa57b7dacd37b777d
rvogel <vogel@hallowelt.biz>
2024-06-18 16:33:56 +0200

    Fix tool download urls

--- _bluespice/pre-autoload-dump.d/10-add_tools.sh
+++ _bluespice/pre-autoload-dump.d/10-add_tools.sh
@@ -10,8 +10,8 @@ then
 	mkdir -p "$targetdir"
 fi
 
-wget -q -O "$targetdir/mediawiki-adm" "https://github.com/hallowelt/misc-mediawiki-adm/releases/latest"
+wget -q -O "$targetdir/mediawiki-adm" "https://github.com/hallowelt/misc-mediawiki-adm/releases/latest/download/mediawiki-adm"
 chmod +x "$targetdir/mediawiki-adm"
 
-wget -q -O "$targetdir/parallel-runjobs-service" "https://github.com/hallowelt/misc-parallel-runjobs-service/releases/latest"
+wget -q -O "$targetdir/parallel-runjobs-service" "https://github.com/hallowelt/misc-parallel-runjobs-service/releases/latest/download/parallel-runjobs-service"
 chmod +x "$targetdir/parallel-runjobs-service"
