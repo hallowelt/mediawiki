620ec4ae98088c92488db665917c70e545a5dec8
Robert Vogel <vogel@hallowelt.biz>
2024-10-15 15:37:01 +0200

    Remove useless user preferences
    ERM38984

--- settings.d/080-BlueSpiceDiscovery.php
+++ settings.d/080-BlueSpiceDiscovery.php
@@ -6,8 +6,13 @@ $GLOBALS['wgSkipSkins'] = [
     'minerva',
     'monobook',
     'timeless',
-    'vector'
+    'vector',
+    'vector-2022'
 ];
 
 $GLOBALS['bsgDiscoveryMetaItemsHeader'] = [ "page-sentence" ];
-$GLOBALS['bsgDiscoveryMetaItemsFooter'] = [ "categories" ];
+$GLOBALS['bsgDiscoveryMetaItemsFooter'] = [ "categories", "rating", "recommendations" ];
+
+$GLOBALS['wgHiddenPrefs'][] = 'skin';
+// BlueSpice discovery is responsive by default
+$GLOBALS['wgHiddenPrefs'][] = 'skin-responsive';
