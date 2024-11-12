d5faee1ee3f49e6c65f28054cd3d4da366bebdb9
Robert Vogel <vogel@hallowelt.biz>
2024-11-12 15:28:06 +0100

    Remove WebDAV extensions from 5.0
    ERM37801
    
    [5.x]

--- _bluespice/pre-autoload-dump.d/01-copy_webdav.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-cp extensions/WebDAV/entrypoints/webdav.php .
--- settings.d/050-BlueSpiceDocumentManager.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-wfLoadExtension( 'WebDAV' );
-#wfLoadExtension( 'BlueSpiceWebDAVMinorSave' );  Experimental, disabled by default, enabled only on request
-wfLoadExtension( 'BlueSpiceWebDAVClientIntegration' );
-
-$wgWebDAVAuthType = 'token';
