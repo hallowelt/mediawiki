57e3468c13f59196d6c46c5f52935ef94910ad5f
Robert Vogel <vogel@hallowelt.biz>
2024-12-20 16:16:41 +0100

    Re-add Extension:Variables

--- settings.d/015-BlueSpiceFreeDistribution.php
+++ settings.d/015-BlueSpiceFreeDistribution.php
@@ -153,6 +153,7 @@ wfLoadExtension( 'PageHeader' );
 wfLoadExtension( 'StandardDialogs' );
 wfLoadExtension( 'TemplateStyles' );
 wfLoadExtension( 'TwoColConflict' );
+wfLoadExtension( 'Variables' );
 
 wfLoadExtension( 'VisualEditor' );
 $GLOBALS['wgDefaultUserOptions']['visualeditor-enable'] = 1;
