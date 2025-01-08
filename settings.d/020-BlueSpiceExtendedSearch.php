d78857ad33337733b7dbceaa49445d94cba6e48d
Robert Vogel <vogel@hallowelt.biz>
2025-01-08 12:17:19 +0100

    Remove BlueSpiceExtendedSearch config; Will be enabled by `bluespice/wiki` container

--- settings.d/020-BlueSpiceExtendedSearch.php
+++
@@ -1,7 +0,0 @@
-<?php
-return; // Disabled. Needs Tomcat
-
-wfLoadExtension( 'BlueSpiceExtendedSearch' );
-
-// Not respected by BlueSpiceExtendedSearch
-$GLOBALS['wgHiddenPrefs'][] = 'searchlimit';
