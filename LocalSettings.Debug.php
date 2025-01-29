e37043471d3b20a60349ed210c01d7924c393100
Hua <75751972+huaj1ng@users.noreply.github.com>
2025-01-29 14:21:49 +0100

    Update log channels in LocalSettings.Debug.php
    Add log channels as much as I can.
    
    [REL1_43, REL1_39, REL1_39-4.5.x, master]

--- LocalSettings.Debug.php
+++ LocalSettings.Debug.php
@@ -1,5 +1,22 @@
 <?php
+/**
+ * A logger exporting logs to a certain channel is defined using 
+ * \MediaWiki\Logger\LoggerFactory::getInstance( 'ChannelName' ).
+ * Legacy code might also use wfDebugLog( 'ChannelName', 'Message' ).
+ * 
+ * We try to collect all channel names here, though this is still a
+ * manually-collected non-exhaustive list that may not be up-to-date.
+ * 
+ * In settings.d/000-Logging.php, $wgMWLoggerDefaultSpi is handled
+ * using those bsgDebugLogGroups as below.
+ * 
+ * The destination of a log can be either a file like $IP/cache/debug.log,
+ * or a stream, e.g php://stdout or php://stderr.
+ */
 
+#$GLOBALS['bsgDebugLogGroups']['AbuseFilter'] = "$IP/cache/debug.log";
+#$GLOBALS['bsgDebugLogGroups']['ActionFactory'] = "$IP/cache/debug.log";
+#$GLOBALS['bsgDebugLogGroups']['ActorStore'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['api'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['api-feature-usage'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['api-request'] = "$IP/cache/debug.log";
@@ -12,18 +29,44 @@
 #$GLOBALS['bsgDebugLogGroups']['baz'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['BlockManager'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['BlueSpice'] = "$IP/cache/debug.log";
+#$GLOBALS['bsgDebugLogGroups']['bluespice'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['BlueSpiceChecklist'] = "$IP/cache/debug.log";
+#$GLOBALS['bsgDebugLogGroups']['bluespice-deprecations'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['bluespicediscovery'] = "$IP/cache/debug.log";
+#$GLOBALS['bsgDebugLogGroups']['BlueSpiceExtendedSearch-string'] = "$IP/cache/debug.log";
+#$GLOBALS['bsgDebugLogGroups']['BlueSpiceExtendedSearch-text'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['BlueSpiceFlaggedRevsConnector'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['BlueSpiceGroupManager'] = "$IP/cache/debug.log";
+#$GLOBALS['bsgDebugLogGroups']['BlueSpiceInstanceStatus'] = "$IP/cache/debug.log";
+#$GLOBALS['bsgDebugLogGroups']['BlueSpiceReaders'] = "$IP/cache/debug.log";
+#$GLOBALS['bsgDebugLogGroups']['BlueSpiceSocial'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['BlueSpiceSocialProfile'] = "$IP/cache/debug.log";
+#$GLOBALS['bsgDebugLogGroups']['BlueSpiceWikiFarm'] = "$IP/cache/debug.log";
+#$GLOBALS['bsgDebugLogGroups']['BS::Bookshelf'] = "$IP/cache/debug.log";
+#$GLOBALS['bsgDebugLogGroups']['BS::Reminder'] = "$IP/cache/debug.log";
+#$GLOBALS['bsgDebugLogGroups']['BS::UEModuleDOCX'] = "$IP/cache/debug.log";
+#$GLOBALS['bsgDebugLogGroups']['BS::UEModuleHTML'] = "$IP/cache/debug.log";
+#$GLOBALS['bsgDebugLogGroups']['BS::UEModulePDF'] = "$IP/cache/debug.log";
+#$GLOBALS['bsgDebugLogGroups']['BS::UniversalExport'] = "$IP/cache/debug.log";
+#$GLOBALS['bsgDebugLogGroups']['BSBookshelf'] = "$IP/cache/debug.log";
+#$GLOBALS['bsgDebugLogGroups']['BSExtendedSearch'] = "$IP/cache/debug.log";
+#$GLOBALS['bsgDebugLogGroups']['BsRoleSystem'] = "$IP/cache/debug.log";
+#$GLOBALS['bsgDebugLogGroups']['BSSocial'] = "$IP/cache/debug.log";
+#$GLOBALS['bsgDebugLogGroups']['BSUsageTracker'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['Bug58676'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['cache-cookies'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['captcha'] = "$IP/cache/debug.log";
+#$GLOBALS['bsgDebugLogGroups']['checkUsernames'] = "$IP/cache/debug.log";
+#$GLOBALS['bsgDebugLogGroups']['CollabPads'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['ContentHandler'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['ContentProvisioner'] = "$IP/cache/debug.log";
+#$GLOBALS['bsgDebugLogGroups']['ContentProvisioner_Sync'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['ContentTransfer'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['cookie'] = "$IP/cache/debug.log";
+#$GLOBALS['bsgDebugLogGroups']['CreateUserPage'] = "$IP/cache/debug.log";
+#$GLOBALS['bsgDebugLogGroups']['csp'] = "$IP/cache/debug.log";
+#$GLOBALS['bsgDebugLogGroups']['csp-report-only'] = "$IP/cache/debug.log";
+#$GLOBALS['bsgDebugLogGroups']['DatabaseBlockStore'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['DBConnection'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['DBPerformance'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['DBQuery'] = "$IP/cache/debug.log";
@@ -31,7 +74,11 @@
 #$GLOBALS['bsgDebugLogGroups']['DeferredUpdates'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['diff'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['DuplicateParse'] = "$IP/cache/debug.log";
+#$GLOBALS['bsgDebugLogGroups']['dynamic-config'] = "$IP/cache/debug.log";
+#$GLOBALS['bsgDebugLogGroups']['EditConflict'] = "$IP/cache/debug.log";
+#$GLOBALS['bsgDebugLogGroups']['EditConstraintRunner'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['editpage'] = "$IP/cache/debug.log";
+#$GLOBALS['bsgDebugLogGroups']['EmbedVideo'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['EventBus'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['exception'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['exception-json'] = "$IP/cache/debug.log";
@@ -44,12 +91,17 @@
 #$GLOBALS['bsgDebugLogGroups']['fileupload'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['FlaggedRevisions'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['foo'] = "$IP/cache/debug.log";
+#$GLOBALS['bsgDebugLogGroups']['formatnum'] = "$IP/cache/debug.log";
+#$GLOBALS['bsgDebugLogGroups']['gitinfo'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['GlobalTitleFail'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['header'] = "$IP/cache/debug.log";
+#$GLOBALS['bsgDebugLogGroups']['headertabs'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['headers-sent'] = "$IP/cache/debug.log";
+#$GLOBALS['bsgDebugLogGroups']['HitCounters'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['http'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['HttpError'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['imagemove'] = "$IP/cache/debug.log";
+#$GLOBALS['bsgDebugLogGroups']['img_auth'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['ImportOfficeFiles'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['ImportOfficeFiles_RemoveOrphanedDirectories'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['ImportOfficeFiles_UI'] = "$IP/cache/debug.log";
@@ -60,18 +112,24 @@
 #$GLOBALS['bsgDebugLogGroups']['JobQueueRedis'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['languageconverter'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['LDAPAuthorization'] = "$IP/cache/debug.log";
+#$GLOBALS['bsgDebugLogGroups']['LDAPAuthorization2'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['LDAPGroups'] = "$IP/cache/debug.log";
+#$GLOBALS['bsgDebugLogGroups']['LDAPProvider'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['LDAPSyncAll'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['LDAPUserInfo'] = "$IP/cache/debug.log";
+#$GLOBALS['bsgDebugLogGroups']['LinkBatch'] = "$IP/cache/debug.log";
+#$GLOBALS['bsgDebugLogGroups']['LinkCache'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['LocalFile'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['localisation'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['LockManager'] = "$IP/cache/debug.log";
+#$GLOBALS['bsgDebugLogGroups']['logentry'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['logging'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['login'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['Math'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']["MediaWikiSite"] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['MessageCache'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['message-format'] = "$IP/cache/debug.log";
+#$GLOBALS['bsgDebugLogGroups']['Metrics'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['Mime'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['MWStakeComponentManifestRegistry'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['NameTableSqlStore'] = "$IP/cache/debug.log";
@@ -80,51 +138,79 @@
 #$GLOBALS['bsgDebugLogGroups']['OAuth'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['objectcache'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['OldRevisionImporter'] = "$IP/cache/debug.log";
+#$GLOBALS['bsgDebugLogGroups']['OpenIDConnect'] = "$IP/cache/debug.log";
+#$GLOBALS['bsgDebugLogGroups']['output'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['parser'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['Parser'] = "$IP/cache/debug.log";
+#$GLOBALS['bsgDebugLogGroups']['ParserCache'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['Parsoid'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['permissionmanager'] = "$IP/cache/debug.log";
+#$GLOBALS['bsgDebugLogGroups']['Pingback'] = "$IP/cache/debug.log";
+#$GLOBALS['bsgDebugLogGroups']['PluggableAuth'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['poolcounter'] = "$IP/cache/debug.log";
+#$GLOBALS['bsgDebugLogGroups']['popups'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['preferences'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['Preprocessor'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['processmanager-code-executer'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['ProcessRunner'] = "$IP/cache/debug.log";
+#$GLOBALS['bsgDebugLogGroups']['profiler'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['ratelimit'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['RCCacheEntryFactory'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['recentchanges'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['RecursiveLinkPurge'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['RedirectedPosts'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['redis'] = "$IP/cache/debug.log";
+#$GLOBALS['bsgDebugLogGroups']['RefreshLinksJob'] = "$IP/cache/debug.log";
+#$GLOBALS['bsgDebugLogGroups']['Renameuser'] = "$IP/cache/debug.log";
+#$GLOBALS['bsgDebugLogGroups']['replication'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['resourceloader'] = "$IP/cache/debug.log";
+#$GLOBALS['bsgDebugLogGroups']['RevertedTagUpdate'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['RevisionStore'] = "$IP/cache/debug.log";
+#$GLOBALS['bsgDebugLogGroups']['RSS'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['rssfeeder'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['runJobs'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['runjobs-trigger-runner'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['RunSingleJobHandler'] = "$IP/cache/debug.log";
+#$GLOBALS['bsgDebugLogGroups']['SavePgae'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['SaveParse'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['Scribunto'] = "$IP/cache/debug.log";
+#$GLOBALS['bsgDebugLogGroups']['search'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['SecondaryDataUpdate'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']["security"] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['session'] = "$IP/cache/debug.log";
+#$GLOBALS['bsgDebugLogGroups']['session-ip'] = "$IP/cache/debug.log";
+#$GLOBALS['bsgDebugLogGroups']['Settings'] = "$IP/cache/debug.log";
+#$GLOBALS['bsgDebugLogGroups']['Setup'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['SimpleSAMLphp'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['slow-parse'] = "$IP/cache/debug.log";
+#$GLOBALS['bsgDebugLogGroups']['slow-parsoid'] = "$IP/cache/debug.log";
+#$GLOBALS['bsgDebugLogGroups']['smw'] = "$IP/cache/debug.log";
+#$GLOBALS['bsgDebugLogGroups']['SpamBlacklist'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['squid'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['StashEdit'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['textextracts'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['throttler'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['thumbnail'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['Timing'] = "$IP/cache/debug.log";
+#$GLOBALS['bsgDebugLogGroups']['TitleBlacklist'] = "$IP/cache/debug.log";
+#$GLOBALS['bsgDebugLogGroups']['TitleBlacklist-cache'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['tocreate'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['tomutate'] = "$IP/cache/debug.log";
+#$GLOBALS['bsgDebugLogGroups']['TrackingCategories'] = "$IP/cache/debug.log";
+#$GLOBALS['bsgDebugLogGroups']['UndeletePage'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['UploadRevisionImporter'] = "$IP/cache/debug.log";
+#$GLOBALS['bsgDebugLogGroups']['UserCache'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['UserGroupManager'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['UserNameUtils'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['UserOptionsManager'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['VEForAll'] = "$IP/cache/debug.log";
+#$GLOBALS['bsgDebugLogGroups']['visualeditor'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['VisualEditor'] = "$IP/cache/debug.log";
+#$GLOBALS['bsgDebugLogGroups']['WebDAV'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['WebP'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['wfDebug'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['wfLogDBError'] = "$IP/cache/debug.log";
+#$GLOBALS['bsgDebugLogGroups']['Widgets'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['wikipage'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']['workflows'] = "$IP/cache/debug.log";
 #$GLOBALS['bsgDebugLogGroups']["WSSlots"] = "$IP/cache/debug.log";
