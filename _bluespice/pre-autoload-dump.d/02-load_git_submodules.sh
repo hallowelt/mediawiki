dccd18ddde0120fbbc42c65a880270664e9fb4bb
Robert Vogel <vogel@hallowelt.biz>
2025-04-23 12:33:03 +0200

    Convert submodules to github

--- _bluespice/pre-autoload-dump.d/02-load_git_submodules.sh
+++ _bluespice/pre-autoload-dump.d/02-load_git_submodules.sh
@@ -7,6 +7,17 @@
 # License: GPLv3
 
 mv vendor vendor_by_composer
-git submodule update --init --recursive
+
+# We want to clone submodules from github, not from Wikimedia gerrit
+# Examples:
+# 	https://gerrit.wikimedia.org/r/mediawiki/extensions/CiteThisPage -> git@github.com:wikimedia/mediawiki-extensions-CiteThisPage
+# 	https://gerrit.wikimedia.org/r/mediawiki/skins/Vector -> git@github.com:wikimedia/mediawiki-skins-Vector
+# 	https://gerrit.wikimedia.org/r/mediawiki/vendor -> git@github.com:wikimedia/mediawiki-vendor
+
+sed -i 's|https://gerrit.wikimedia.org/r/mediawiki/|git@github.com:wikimedia/mediawiki-|g' .gitmodules
+sed -i 's|mediawiki-extensions/|mediawiki-extensions-|g' .gitmodules
+sed -i 's|mediawiki-skins/|mediawiki-skins-|g' .gitmodules
+
+git submodule update --init
 rm -rf vendor
 mv vendor_by_composer vendor
