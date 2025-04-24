419798e80739ed41428263db40c37d74eb045941
Robert Vogel <vogel@hallowelt.biz>
2025-04-24 10:42:58 +0200

    Sync with REL1_43-5.0.x

--- _bluespice/pre-autoload-dump.d/02-load_git_submodules.sh
+++ _bluespice/pre-autoload-dump.d/02-load_git_submodules.sh
@@ -7,6 +7,29 @@
 # License: GPLv3
 
 mv vendor vendor_by_composer
-git submodule update --init --recursive
+
+# We want to clone submodules from github, not from Wikimedia gerrit
+# Examples:
+# 	https://gerrit.wikimedia.org/r/mediawiki/extensions/CiteThisPage -> git@github.com:wikimedia/mediawiki-extensions-CiteThisPage
+# 	https://gerrit.wikimedia.org/r/mediawiki/skins/Vector -> git@github.com:wikimedia/mediawiki-skins-Vector
+# 	https://gerrit.wikimedia.org/r/mediawiki/vendor -> git@github.com:wikimedia/mediawiki-vendor
+
+sed -i 's|https://gerrit.wikimedia.org/r/mediawiki/|git@github.com:wikimedia/mediawiki-|g' .gitmodules
+sed -i 's|mediawiki-extensions/|mediawiki-extensions-|g' .gitmodules
+sed -i 's|mediawiki-skins/|mediawiki-skins-|g' .gitmodules
+
+git submodule init
+# Only clone with depth 1
+submodules=$(git config --file .gitmodules --get-regexp path | awk '{ print $2 }')
+for submodule in $submodules; do
+	git submodule update --depth 1 -- $submodule
+done
+
+# Special handling for submodule in Extension:VisualEditor
+sed -i 's|https://gerrit.wikimedia.org/r/VisualEditor/VisualEditor|git@github.com:wikimedia/VisualEditor|g' extensions/VisualEditor/.gitmodules
+cd extensions/VisualEditor
+git submodule update --init --depth 1 lib/ve
+cd -
+
 rm -rf vendor
 mv vendor_by_composer vendor
