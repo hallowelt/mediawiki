e2876025d2c53480e6aaa922363b9c0b895382e2
hslater <hamish.slater@hallowelt.com>
2025-04-28 12:01:13 +1200

    Exclude upload log from log restrictions
    ERM41297
    
    [REL1_43, REL1_43-5.0.x, master]

--- settings.d/020-DefaultSettings.php
+++ settings.d/020-DefaultSettings.php
@@ -46,6 +46,9 @@ $GLOBALS['wgExtensionFunctions'][] = function() {
 $GLOBALS['wgExtensionFunctions'][] = function() {
 	$logKeys = $GLOBALS['wgLogTypes'];
 	foreach ( $logKeys as $logKey ) {
+		if ( $logKey === 'upload' ) {
+			continue;
+		}
 		if ( !isset( $GLOBALS['wgLogRestrictions'][$logKey] ) ) {
 			$GLOBALS['wgLogRestrictions'][$logKey] = 'wikiadmin';
 		}
