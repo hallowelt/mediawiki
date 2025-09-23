bf9fa735d512b329988fe9301909c076652add38
Hua Jing <hua.jing@gmx.de>
2025-09-23 19:55:52 +0000

    [hua] remove PageHeader
    as it is in private repository and blocks composer update

--- settings.d/030-BlueSpiceFreeDistribution.php
+++ settings.d/030-BlueSpiceFreeDistribution.php
@@ -58,7 +58,6 @@ wfLoadExtension( 'NotifyMe' );
 wfLoadExtension( 'Echo', $GLOBALS['IP'] . '/extensions/BlueSpiceDistributionConnector/extension-shims/Echo/extension.json' );
 
 wfLoadExtension( 'OOJSPlus' );
-wfLoadExtension( 'PageHeader' );
 wfLoadExtension( 'PDFCreator' );
 wfLoadExtension( 'PdfHandler' );
 wfLoadExtension( 'RSS' );
