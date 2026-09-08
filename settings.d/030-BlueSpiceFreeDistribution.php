92f2034f55a2b7bbdd02c5140dbc55943fb4b8df
xXJonney <76098234+xXJonney@users.noreply.github.com>
2026-09-08 10:22:32 +0200

    Enable new watchlist UI
    ERM50065
    
    [5.x]

--- settings.d/030-BlueSpiceFreeDistribution.php
+++ settings.d/030-BlueSpiceFreeDistribution.php
@@ -17,6 +17,7 @@ $GLOBALS[ 'wgEnhancedUIsAllPagesOverride' ] = true;
 $GLOBALS[ 'wgEnhancedUIsFilelistOverride' ] = true;
 $GLOBALS[ 'wgEnhancedUIsSpecialSpecialPagesOverride' ] = true;
 $GLOBALS[ 'wgEnhancedUIsSpecialPreferencesOverride' ] = true;
+$GLOBALS[ 'wgEnhancedUIsWatchlistOverride' ] = true;
 # Some MediaWiki user prefences are confusing and/or incompatible with BlueSpice.
 # Therefore we hide them in the new "enhanced" user preferences of Extension EnhancedStandardUIs.
 # Section:Info
